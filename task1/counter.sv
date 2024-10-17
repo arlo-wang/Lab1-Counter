2c24bc67d9e14f7551ff299fdb184e90507a5c72
arlo-wang <arlowang23@gmail.com>
2024-10-18 00:51:16 +0100

    lab1 before step 6

--- task1/counter.sv
+++ task1/counter.sv
@@ -4,11 +4,11 @@ module counter #(
     input  logic             clk,    // clock
     input  logic             rst,    // reset
     input  logic             en,     // counter enable
-    output logic [WIDTH-1;0] count   // count output
+    output logic [WIDTH-1:0] count   // count output
 );
     
 always_ff @ (posedge clk) begin
     if (rst) count <= {WIDTH{1'b0}};
-    else     count <= count + {{WIDTH{1'b0}}, en};
+    else     count <= count + {{WIDTH-1{1'b0}}, en};
 end
 endmodule
---
+++ task1/counter_tb.cpp
@@ -0,0 +1,70 @@
+// Mandatory header files
+// Vcounter.h for the module counter
+
+#include "Vcounter.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+// #include "vbuddy.cpp"
+
+int main(int argc, char **argv, char **env) {
+    // i counts the number of clock cycles to simulate.
+    // clk is the module clock signal.
+    int i;
+    int clk;
+
+    Verilated::commandArgs(argc, argv);
+
+    // init top verilaog instance
+
+    /*
+    instantiate the counter module as Vcounter, 
+    which  is the name of all generated fiels.
+    This is the DUT! 
+    */
+    Vcounter*top = new Vcounter;
+
+    // init trace dump
+
+    /*
+    Turn on signal tracing,
+    and tell Verilator to dump the waveform data to counter.vcd
+    */
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace (tfp, 99);
+    tfp->open ("counter.vcd");
+
+    // initialize simulation inputs
+    
+    /*
+    set initial signal levels
+    top is the name of the top-level entity
+    only the top-level signals are visible
+    */
+    top->clk = 1;
+    top->rst = 1;
+    top->en = 0;
+
+    // run simulation for many clock cycles 
+    for (i = 0; i < 300; i++) {  // this is the for loop where simulations happens
+
+        // dump variables into VCD file and toggle clock
+
+        /*
+        This is the loop that toggles the clock.
+        It also output the trace for each half of the clock cycle, 
+        and force the model to evaluate on both edges of the clock
+        */
+        for (clk = 0; clk < 2; clk++) {
+            tfp->dump (2*i+clk);  // unit is in ps!!!
+            top->clk = !top->clk;
+            top->eval ();
+        }
+        // Change rst and en signals during simulation
+        top->rst = (i < 2) | (i == 15);
+        top->en = (i > 4);
+        if (Verilated::gotFinish())  exit(0);
+    }
+    tfp->close();
+    exit(0);
+}
