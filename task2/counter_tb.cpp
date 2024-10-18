bf7a2ae8978e9d006f96debf1c945be1604d7843
arlo-wang <arlowang23@gmail.com>
2024-10-18 11:31:22 +0100

    end of the lab

---
+++ task2/counter_tb.cpp
@@ -0,0 +1,66 @@
+// Mandatory header files
+// Vcounter.h for the module counter
+
+#include "Vcounter.h"
+#include "/usr/local/share/verilator/include/verilated.h"
+#include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
+
+int main(int argc, char **argv, char **env) {
+    // i counts the number of clock cycles to simulate.
+    // clk is the module clock signal.
+    int i;
+    int clk;
+
+    Verilated::commandArgs(argc, argv);
+
+    // init top verilaog instance
+    Vcounter*top = new Vcounter;
+    // init trace dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace (tfp, 99);
+    tfp->open ("counter.vcd");
+
+    // init Vbuddy
+    /*
+    Open and initialize Vbuddy connection.
+    Port path is in vbuddy.cfg
+    */
+    if (vbdOpen()!=1)  return(-1);
+    vbdHeader("Lab 1: Counter");
+
+    // initialize simulation inputs
+    top->clk = 1;
+    top->rst = 1;
+    top->en = 0;
+
+    // run simulation for many clock cycles 
+    for (i = 0; i < 300; i++) {  // this is the for loop where simulations happens
+
+        // dump variables into VCD file and toggle clock
+        for (clk = 0; clk < 2; clk++) {
+            tfp->dump (2*i+clk);  // unit is in ps!!!
+            top->clk = !top->clk;
+            top->eval ();
+        }
+
+        // ++++  Send count value to Vbuddy
+        vbdHex(4, (int(top->count) >> 16) & 0xF);
+        vbdHex(3, (int(top->count) >> 8) & 0xF);
+        vbdHex(2, (int(top->count) >> 4) & 0xF);
+        vbdHex(1, int(top->count) & 0xF);
+        vbdCycle(i+1);
+        // ---- end of Vbuddy output section 
+
+        // Change rst and en signals during simulation
+        top->rst = (i < 2) | (i == 15);
+        top->en = (i > 4);
+        if (Verilated::gotFinish())  exit(0);
+    }
+
+    // House keeping
+    vbdClose();   // ++++
+    tfp->close();
+    exit(0);
+}
